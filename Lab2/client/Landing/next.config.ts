f639e2399721cbbd1e52bcdf34fb2bede1058134
cathaleia <eficiencia.desarrollo@copowe.com.co>
2025-04-15 16:28:58 -0500

    reconstruyendo

--- Lab2/client/Landing/next.config.ts
+++ Lab2/client/Landing/next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    // Disable Next.js optimizations for images (it's not available for static sites
-    // and will throw an error)
-    unoptimized: true,
-  },
 
   // This tells Next.js to export a static build to the `out` folder
   output: "export",
