d00aaacb4b0e4533309c850796a000053efa9895
cathaleia <eficiencia.desarrollo@copowe.com.co>
2025-04-15 19:44:40 -0500

    nos devolvemos a la version V3 para volver a desplegar python3.8

--- Lab2/scripts/deployment.sh
+++ Lab2/scripts/deployment.sh
@@ -24,8 +24,12 @@ while [[ "$#" -gt 0 ]]; do
   shift
 done
 
-
 # During AWS hosted events using event engine tool
+# we pre-provision cloudfront and s3 buckets which hosts UI code.
+# So that it improves this labs total execution time.
+# Below code checks if cloudfront and s3 buckets are
+# pre-provisioned or not and then concludes if the workshop
+# is running in AWS hosted event through event engine tool or not.
 IS_RUNNING_IN_EVENT_ENGINE=false
 PREPROVISIONED_ADMIN_SITE=$(aws cloudformation list-exports --query "Exports[?Name=='Serverless-SaaS-AdminAppSite'].Value" --output text)
 if [ ! -z "$PREPROVISIONED_ADMIN_SITE" ]; then
@@ -40,38 +44,53 @@ fi
 if [[ $server -eq 1 ]]; then
   echo "Server code is getting deployed"
 
-  cd ../server || exit
+  cd ../server || exit # stop execution if cd fails
+  # REGION=$(aws configure get region)
   REGION=$(aws configure get region || echo "us-east-1")
   DEFAULT_SAM_S3_BUCKET=$(grep s3_bucket samconfig.toml | cut -d'=' -f2 | cut -d \" -f2)
-  
-  echo "Validating S3 bucket: $DEFAULT_SAM_S3_BUCKET"
+  echo "aws s3 ls s3://$DEFAULT_SAM_S3_BUCKET"
   if ! aws s3 ls "s3://${DEFAULT_SAM_S3_BUCKET}"; then
-    echo "Creating new S3 bucket..."
+    echo "S3 Bucket: $DEFAULT_SAM_S3_BUCKET specified in samconfig.toml is not readable.
+      So creating a new S3 bucket and will update samconfig.toml with new bucket name."
+
     UUID=$(uuidgen | awk '{print tolower($0)}')
     SAM_S3_BUCKET=sam-bootstrap-bucket-$UUID
     aws s3 mb "s3://${SAM_S3_BUCKET}" --region "$REGION"
     aws s3api put-bucket-encryption \
       --bucket "$SAM_S3_BUCKET" \
       --server-side-encryption-configuration '{"Rules": [{"ApplyServerSideEncryptionByDefault": {"SSEAlgorithm": "AES256"}}]}'
-    
-    # Actualizar configuraciones SAM
-    find ../../ -name "*-samconfig.toml" -exec sed -i "s/s3_bucket = .*/s3_bucket = \"$SAM_S3_BUCKET\"/" {} \;
+    if [[ $? -ne 0 ]]; then
+      exit 1
+    fi
+    # Updating all labs samconfig.toml with new bucket name
+    sed -i "s/s3_bucket = .*/s3_bucket = \"$SAM_S3_BUCKET\"/" samconfig.toml
+    sed -i "s/s3_bucket = .*/s3_bucket = \"$SAM_S3_BUCKET\"/" ../../Lab3/server/shared-samconfig.toml
+    sed -i "s/s3_bucket = .*/s3_bucket = \"$SAM_S3_BUCKET\"/" ../../Lab3/server/tenant-samconfig.toml
+    sed -i "s/s3_bucket = .*/s3_bucket = \"$SAM_S3_BUCKET\"/" ../../Lab4/server/shared-samconfig.toml
+    sed -i "s/s3_bucket = .*/s3_bucket = \"$SAM_S3_BUCKET\"/" ../../Lab4/server/tenant-samconfig.toml
+    sed -i "s/s3_bucket = .*/s3_bucket = \"$SAM_S3_BUCKET\"/" ../../Lab5/server/shared-samconfig.toml
+    sed -i "s/s3_bucket = .*/s3_bucket = \"$SAM_S3_BUCKET\"/" ../../Lab5/server/tenant-samconfig.toml
+    sed -i "s/s3_bucket = .*/s3_bucket = \"$SAM_S3_BUCKET\"/" ../../Lab6/server/shared-samconfig.toml
+    sed -i "s/s3_bucket = .*/s3_bucket = \"$SAM_S3_BUCKET\"/" ../../Lab6/server/tenant-samconfig.toml
     sed -i "s/s3_bucket = .*/s3_bucket = \"$SAM_S3_BUCKET\"/" ../../Lab7/samconfig.toml
   fi
 
-
-  # sam build -t template.yaml --use-container
-  sam build -t template.yaml
-
-  if [ "$IS_RUNNING_IN_EVENT_ENGINE" = true ]; then
-    sam deploy --config-file samconfig.toml --region="$REGION" \
-      --parameter-overrides EventEngineParameter=$IS_RUNNING_IN_EVENT_ENGINE AdminUserPoolCallbackURLParameter=$ADMIN_SITE_URL
-  else
-    sam deploy --config-file samconfig.toml --region="$REGION" \
-      --parameter-overrides EventEngineParameter=$IS_RUNNING_IN_EVENT_ENGINE
+  echo "Validating server code using pylint"
+python3 -m pylint -E -d E0401 $(find . -iname "*.py" -not -path "./.aws-sam/*" -not -path "./layers/*")
+  if [[ $? -ne 0 ]]; then
+    echo "****ERROR: Please fix above code errors and then rerun script!!****"
+    exit 1
   fi
 
-  cd ../scripts || exit
+  sam build -t template.yaml --use-container
+
+  if [ "$IS_RUNNING_IN_EVENT_ENGINE" = true ]; then
+    sam deploy --config-file samconfig.toml --region="$REGION" --parameter-overrides EventEngineParameter=$IS_RUNNING_IN_EVENT_ENGINE AdminUserPoolCallbackURLParameter=$ADMIN_SITE_URL
+  else
+    sam deploy --config-file samconfig.toml --region="$REGION" --parameter-overrides EventEngineParameter=$IS_RUNNING_IN_EVENT_ENGINE
+  fi
+
+  cd ../scripts || exit # stop execution if cd fails
 fi
 
 if [ "$IS_RUNNING_IN_EVENT_ENGINE" = false ]; then
@@ -84,6 +103,7 @@ fi
 if [[ $client -eq 1 ]]; then
   if [[ -z "$email" ]]; then
     echo "Please provide email address to setup an admin user"
+    echo "Note: Invoke script without parameters to know the list of script parameters"
     exit 1
   fi
   echo "Client code is getting deployed"
@@ -93,22 +113,32 @@ if [[ $client -eq 1 ]]; then
   ADMIN_USERPOOL_ID=$(aws cloudformation describe-stacks --stack-name serverless-saas --query "Stacks[0].Outputs[?OutputKey=='CognitoOperationUsersUserPoolId'].OutputValue" --output text)
   ADMIN_USER_GROUP_NAME=$(aws cloudformation describe-stacks --stack-name serverless-saas --query "Stacks[0].Outputs[?OutputKey=='CognitoAdminUserGroupName'].OutputValue" --output text)
 
-  # Configurar usuario admin
-  aws cognito-idp admin-create-user \
+  # Create admin-user in OperationUsers userpool with given input email address
+  CREATE_ADMIN_USER=$(aws cognito-idp admin-create-user \
+  --user-pool-id "$ADMIN_USERPOOL_ID" \
+  --username admin-user \
+  --user-attributes Name=email,Value="$email" Name=email_verified,Value="True" Name=phone_number,Value="+11234567890" Name="custom:userRole",Value="SystemAdmin" Name="custom:tenantId",Value="system_admins" \
+  --desired-delivery-mediums EMAIL)
+
+  echo "$CREATE_ADMIN_USER"
+
+  # Add admin-user to admin user group
+  ADD_ADMIN_USER_TO_GROUP=$(aws cognito-idp admin-add-user-to-group \
     --user-pool-id "$ADMIN_USERPOOL_ID" \
     --username admin-user \
-    --user-attributes Name=email,Value="$email" Name=email_verified,Value="True" \
-    Name=phone_number,Value="+11234567890" Name="custom:userRole",Value="SystemAdmin" \
-    Name="custom:tenantId",Value="system_admins" \
-    --desired-delivery-mediums EMAIL
+    --group-name "$ADMIN_USER_GROUP_NAME")
 
-  aws cognito-idp admin-add-user-to-group \
-    --user-pool-id "$ADMIN_USERPOOL_ID" \
-    --username admin-user \
-    --group-name "$ADMIN_USER_GROUP_NAME"
+  echo "$ADD_ADMIN_USER_TO_GROUP"
 
-  # Despliegue Admin UI
-  cd ../client/Admin || exit
+  # Configuring admin UI
+
+  echo "aws s3 ls s3://$ADMIN_SITE_BUCKET"
+  if ! aws s3 ls "s3://${ADMIN_SITE_BUCKET}"; then
+    echo "Error! S3 Bucket: $ADMIN_SITE_BUCKET not readable"
+    exit 1
+  fi
+
+  cd ../client/Admin || exit # stop execution if cd fails
 
   echo "Configuring environment for Admin Client"
   cat <<EoF >./src/environments/environment.prod.ts
@@ -141,21 +171,49 @@ EoF
   echo "aws s3 sync --delete --cache-control no-store dist s3://${ADMIN_SITE_BUCKET}"
   aws s3 sync --delete --cache-control no-store dist "s3://${ADMIN_SITE_BUCKET}"
 
-  # Despliegue Landing UI (Next.js)
-  cd ../
-  cd Landing || exit
+  if [[ $? -ne 0 ]]; then
+    exit 1
+  fi
 
-  echo "Configuring environment for Landing Client"
-  cat <<EoF >.env.production
-NEXT_PUBLIC_API_GATEWAY_URL="$ADMIN_APIGATEWAYURL"
-NEXT_PUBLIC_AWS_REGION="$REGION"
-EoF
+  echo "Completed configuring environment for Admin Client"
 
-  npm install && npm run build
-  aws s3 sync --delete --cache-control no-store out "s3://${LANDING_APP_SITE_BUCKET}"
+  # Configuring landing UI
 
-  echo "Deployment completed successfully"
-fi
+  echo "aws s3 ls s3://${LANDING_APP_SITE_BUCKET}"
+  if ! aws s3 ls "s3://${LANDING_APP_SITE_BUCKET}"; then
+    echo "Error! S3 Bucket: $LANDING_APP_SITE_BUCKET not readable"
+    exit 1
+  fi
 
+#   cd ../
+
+#   cd Landing || exit # stop execution if cd fails
+
+#   echo "Configuring environment for Landing Client"
+
+#  # Crear archivo .env.production con variables necesarias
+#   cat <<EoF >.env.production
+# NEXT_PUBLIC_API_GATEWAY_URL="$ADMIN_APIGATEWAYURL"
+# NEXT_PUBLIC_AWS_REGION="$REGION"
+# EoF
+
+#   # Crear archivo .env.development para uso local (opcional)
+#   cat <<EoF >.env.development
+# NEXT_PUBLIC_API_GATEWAY_URL="$ADMIN_APIGATEWAYURL"
+# NEXT_PUBLIC_AWS_REGION="us-east-1"
+# EoF
+
+#   npm install && npm run build
+
+#   echo "aws s3 sync --delete --cache-control no-store ./out s3://${LANDING_APP_SITE_BUCKET}"
+#   aws s3 sync --delete --cache-control no-store ./out "s3://${LANDING_APP_SITE_BUCKET}"
+
+#   if [[ $? -ne 0 ]]; then
+#     exit 1
+#   fi
+
+#   echo "Completed configuring environment for Landing Client"
+#   echo "Successfully completed deploying Admin UI and Landing UI"
+# fi
 echo "Admin site URL: https://$ADMIN_SITE_URL"
 echo "Landing site URL: https://$LANDING_APP_SITE_URL"
